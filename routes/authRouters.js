b14e63647f95beeeea859c657b0da0ab0a8f4f75
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-02-22 21:18:41 -0500

    fifth commit

--- routes/authRouters.js
+++ routes/authRouters.js
@@ -1,64 +1,105 @@
 import express from "express";
-import { prisma } from "../db";
+import { prisma } from "../db/index.js";
+import jwt from "jsonwebtoken";
 import argon2 from "argon2";
 
-const router = express.Router()
 
-router.post("/signup", (request, response) => {
+const router = express.Router();
 
-    try{
+//CREATED SIGNUP
+router.post("/signup", async (request, response) => {
+  try {
+    const foundUser = await prisma.user.findFirst({
+      where: {
+        username: request.body.username,
+      },
+    });
 
-        const foundUser = await prisma.user.findFirst({
-            where:{
+    if(foundUser) {
+      response.status(401).json({
+        success: false,
+        message: "User already exist",
+      });
+    } else {
+      try {
+        const hashPassword = await argon2.hash(request.body.password);
+
+        const newUser = await prisma.user.create({
+          data: {
+            username: request.body.username,
+            password: hashPassword,
+          },
+        });
+
+        if (newUser) {
+          response.status(201).json({
+            success: true,
+            message: "User created successfully",
+          });
+        } else {
+          response.status(500).json({
+            success: false,
+            message: "User was not created", 
+          });
+        }
+      } catch (error) {
+        response.status(500).json({
+          success: false,
+          message: "User was not created",
+        });
+      }
+    }
+  } catch (error) {
+    response.status(500).json({
+      success: false,
+      message: "User was not created",
+    });
+  }
+});
+
+
+//CREATED LOGIN
+router.post("/login", async (request, response) => {
+    try {
+        const user = await prisma.user.findFirst({
+            where: {
                 username: request.body.username,
             }
-            
-        })
+        });
 
-        if(foundUser){
-            response.status.(401).json({
-                success: false,
-                message: "User already exist"
-            });
-        }else{
+        if(foundUser){ //Ask questions about this part
+            try {
+            const verifyPassword = await argon2.verify(
+                foundUser.password, 
+                request.body.password
+                );   
             
-            try{
-                const hashPassword = await argon2.hash(request.body.password)
+            if(verifyPassword===true){
+                const token = jwt.sign({
+                    id:foundUser.id,
+                    username:foundUser.username,
+                }, 
+                    "thisIsASecretKey")
 
-                const newUser = await prisma.user.create({
-                    data: {
-                        username: request.body.username,
-                        password: hashPassword,
-                    },
+                response.status(200).json({
+                    success: true,
+                    token: token,
                 });
-
-                if(newUser){
-                    response.status(201).json({
-                        success: true,
-                        message: "User created"
-                    });
-                }else {
-                    response.status(500).json({
-                        success: false,
-                        message: "User was not created"
-                    });
-                }
-
-            }catch (error){
-                response.status(500).json({
+            } else {
+                response.status(401).json({
                     success: false,
-                    message: "User was not created"
+                    message: "Wrong username or password",
                 });
             }
+        }catch (error) {
+            
         }
-
-    }catch (error){
+    }
+    } catch(error){
         response.status(500).json({
             success: false,
-            message: "User was not created"
+            message: "Something went wrong",
         });
     }
-
 })
-
 export default router;
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import recipeRouter from "./routes/recipe.js"
 import authRouter from "./routes/authRouters.js"
+import passport from "passport";
+import setupJWTStrategy from "./auth/index.js"; 
 
 export default async function createServer(){
     const app = express();
 
     app.use(express.json());
     
+    setupJWTStrategy(passport);
     app.use("auth/", authRouter());
-    app.use("/recipe", recipeRouter())
+    app.use("/recipe", 
+    passport.authenticate("jwt", { session: false, }), recipeRouter())
 
     return app;
 }
